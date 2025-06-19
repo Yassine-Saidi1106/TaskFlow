9caa303b825c25d068b5b8c5bdda5c3413624b8e
Yassine <yassine.saidi@polytechnicien.tn>
2025-06-19 15:21:05 +0100

    config http + creation de serveur

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const http = require ("http") //importation du protocol web
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -31,6 +34,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json('error');
 });
 
+const server = http.createServer (app)
+server.listen(5000, () => {
+    console.log("app is running on port 5000");
+});
